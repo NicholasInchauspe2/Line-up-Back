8982353e97e6243ebe85740cd8c2b1ddfa6bb8d4
MatiasOrlando <matiassorlando@gmail.com>
2023-04-24 14:23:05 +0200

    config mongoAtlas

--- api/db/index.js
+++ api/db/index.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/test", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.5nlimpd.mongodb.net/test`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("DB Online");
   } catch (error) {
     throw new Error("Error a la hora de inicializar DB");
